8d86b21c898f2c1922adce5ca417cec5422e5c96
nahashon <nashonmwendwa0@gmail.com>
2025-07-03 12:46:37 +0300

    Define routes for login tracking (index, non-logged-in, add, delete)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginTrackingController;
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Login Tracking Routes
+|--------------------------------------------------------------------------
+| These routes handle user login tracking views and operations, including:
+| - Viewing login data over a time range
+| - Viewing users who haven’t logged in
+| - Adding new users manually
+| - Deleting users
+*/
+
+Route::prefix('login-tracking')->group(function () {
+    
+    // Main dashboard: shows login counts, last login, etc.
+    Route::get('/', [LoginTrackingController::class, 'index'])
+        ->name('login-tracking.index');
+
+    // Shows list of users who have NOT logged in within selected days
+    Route::get('/non-logged-in', [LoginTrackingController::class, 'nonLoggedInUsers'])
+        ->name('login-tracking.non-logged-in');
+
+    // Manually add a new user to the system
+    Route::post('/store', [LoginTrackingController::class, 'storeUser'])
+        ->name('login-tracking.store');
+
+    // Remove a user from the system
+    Route::delete('/{id}', [LoginTrackingController::class, 'destroyUser'])
+        ->name('login-tracking.destroy');
 });
